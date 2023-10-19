d84bc5787e043effe267292e0ef0f8f4764dabef
Diego Hurtado <ocelotl@users.noreply.github.com>
2023-10-19 08:44:57 -0700

    Add test case

---
+++ python/test_otlp_trace_exporter.py
@@ -0,0 +1,30 @@
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import (
+   OTLPSpanExporter
+)
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import SimpleSpanProcessor
+
+resource = Resource(attributes={
+    SERVICE_NAME: "your-service-name"
+})
+
+provider = TracerProvider(resource=resource)
+processor = SimpleSpanProcessor(
+    OTLPSpanExporter(endpoint="your-endpoint-here")
+)
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+tracer = trace.get_tracer("tracer_name")
+
+
+def test_case():
+
+    with tracer.start_as_current_span("0"):
+        with tracer.start_as_current_span("1"):
+            with tracer.start_as_current_span("2") as span:
+                print("done")
+
+    processor.on_end(span)
