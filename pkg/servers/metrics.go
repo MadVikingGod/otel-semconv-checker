e6dd3155d38ea62b6feb26ca51cab8d8e7ff0d84
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-12-14 08:51:26 -0600

    Fix metrics too

--- pkg/servers/metrics.go
+++ pkg/servers/metrics.go
@@ -35,10 +35,11 @@ func NewMetricsService(cfg Config, svs map[string]semconv.SemanticVersion) *Metr
 	}
 	matches := []matchDef{}
 	for _, match := range cfg.Metrics {
-		if match.SemanticVersion == "" {
+		groups, ok := svs[match.SemanticVersion]
+		if !ok {
 			match.SemanticVersion = semconv.DefaultVersion
 		}
-		matches = append(matches, newMatchDef(match, svs[match.SemanticVersion].Groups))
+		matches = append(matches, newMatchDef(match, groups.Groups))
 	}
 
 	return &MetricsServer{
