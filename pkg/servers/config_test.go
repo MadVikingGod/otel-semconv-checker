cddd11e46536f6c3dca044fd3e880a96f4a12059
Aaron Clawson <3766680+MadVikingGod@users.noreply.github.com>
2023-12-18 14:23:31 -0600

    Make the server tests actually parse the default config (#52)

--- pkg/servers/config_test.go
+++ pkg/servers/config_test.go
@@ -5,14 +5,24 @@ package servers
 import (
 	"testing"
 
+	"github.com/mitchellh/mapstructure"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"gopkg.in/yaml.v3"
 )
 
 func TestDefaultConfig(t *testing.T) {
 	cfg := Config{}
-	err := yaml.Unmarshal([]byte(DefaultConfig), &cfg)
+	tmpCfg := map[string]interface{}{}
+	err := yaml.Unmarshal([]byte(DefaultConfig), &tmpCfg)
+	require.NoError(t, err)
+	err = mapstructure.Decode(tmpCfg, &cfg)
 	assert.NoError(t, err)
+
+	assert.Equal(t, cfg.ServerAddress, "0.0.0.0:4317")
+	assert.GreaterOrEqual(t, len(cfg.Trace), 1)
+	assert.Equal(t, cfg.Trace[0].Match, "http.server.*")
+	assert.GreaterOrEqual(t, len(cfg.Trace[0].Groups), 1)
 
 	assert.NotPanics(t, func() {
 		NewTraceService(cfg, nil)
